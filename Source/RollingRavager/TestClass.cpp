123d81b40f3b59c2b342ba9c531a5bff47ea0c70
Clavus <joerivdvelden@gmail.com>
2015-03-10 01:33:46 +0100

    Initial commit

---
+++ Source/RollingRavager/TestClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RollingRavager.h"
+#include "TestClass.h"
+
+TestClass::TestClass()
+{
+}
+
+TestClass::~TestClass()
+{
+}
